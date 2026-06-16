ca5014219eb3155621ce44217372dae89ece8b52
facundo4512 <facundovallejos2017@outlook.es>
2026-06-16 08:55:16 -0300

    Implementar registro para cambios de color en semáforos
    además en esta función se usa/aplica la librería local-time en la función formato-localtime, que hace que sea más legible para el usuario la hora, ya que sin eso el usuario seguiria viendo en tiempo en unix epoch

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,1 +1,17 @@
 (ql:quickload "local-time") ;carga del archivo "localtime" de la libreria 
+
+;; Requerimiento 3
+;; ======================================================== 
+;; FUNCIÓN: logging
+;; NATURALEZA: Impura (produce salida mediante el format)
+;; ESTRATEGIA: Condicional (Implementada con el uso del COND para la evaluacion del estado del semaforo devuelto por la funcion "tiempo-color")
+;; IMPACTO: No destructivas (no modifica argumento)
+;; ========================================================
+
+(defun analisis-ciclos (tiempo) ; forma del tiempo epoch
+	(cond 
+		((equal (symbol-name (tiempo-color tiempo)) "VERDE") (format nil "Tiempo <~a>: La luz a cambiado de rojo a verde" (formato-localtime tiempo)))
+		((equal (symbol-name (tiempo-color tiempo)) "AMARILLO") (format nil "Tiempo <~a>: La luz a cambiado de verde a amarillo " (formato-localtime tiempo)))
+		((equal (symbol-name (tiempo-color tiempo)) "ROJO") (format nil "Tiempo <~a>: La luz a cambiado de amarrilo a rojo" (formato-localtime tiempo)))
+	)
+)
